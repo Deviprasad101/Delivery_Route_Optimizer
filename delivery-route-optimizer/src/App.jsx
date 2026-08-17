8734e8f702202f8489c6d31b03f7a027246be163
Deviprasad101 <cdpreddyap@gmail.com>
2026-08-17 11:35:46 +0530

    Initial commit with Rider Registration UI

---
+++ delivery-route-optimizer/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import RiderRegistration from './RiderRegistration'
+import './App.css'
+
+function App() {
+  return (
+    <RiderRegistration />
+  )
+}
+
+export default App
